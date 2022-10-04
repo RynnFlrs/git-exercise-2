b88323c2ff387a5e5f19e71011eb0d3dfd5177cf
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-04 22:33:40 +0800

    Created square, farenheit, rectangleArea, and is Palindrome functions

---
+++ lib.js
@@ -0,0 +1,18 @@
+const square = (n) => {
+    return n * n;
+}
+
+const fahrenheit = (celsius) => {
+    return (celsius * 1.8) + 32;
+}
+
+const rectangleArea = (a, b) => {
+    return area = a * b;
+}
+
+const isPalindrome = (str) => {
+    const convertedStr = str.split('').reverse().join('');
+    
+    return str.toLowerCase() === convertedStr.toLowerCase() ? true : false;
+}
+ 
