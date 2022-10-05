6d7cbffaa549e7f18f9dd7edf9b32b7d7addfcf2
Ryann Flores <102124511+RynnFlrs@users.noreply.github.com>
2022-10-05 10:57:28 +0800

    Export Module Testing (#2)
    Created lib.js functions testing using export

---
+++ index.js
@@ -0,0 +1,7 @@
+const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
+
+console.log(`The square of number 11 is ${square(11)}
+The Fahrenheit value of 11 degree celsius is ${fahrenheit(11)}
+A length with 5, and width with 10, has a Rectangle Area of ${rectangleArea(5, 10)}
+The word "SiteMinder" is a Palindrome ? ${isPalindrome('SiteMinder')}
+The word "Wow" is a Palindrome ? ${isPalindrome('Wow')}`);
--- lib.js
+++ lib.js
@@ -5,3 +5,10 @@ const fahrenheit = (celsius) => (celsius * 1.8) + 32;
 const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+
+module.exports = {
+    square,
+    fahrenheit,
+    rectangleArea,
+    isPalindrome
+}
