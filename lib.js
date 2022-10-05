a41eedcd9b22f85e8db261eddcc3433d6c795113
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-05 08:31:04 +0800

    Removed the ternary operator on isPalindrome function

--- lib.js
+++ lib.js
@@ -2,6 +2,6 @@ const square = (n) => Math.pow(n, 2);
 
 const fahrenheit = (celsius) => (celsius * 1.8) + 32;
 
-const rectangleArea = (a, b) =>  area = a * b;
+const rectangleArea = (a, b) => a * b;
 
-const isPalindrome = (str) => str.toLowerCase().replace(" ","") === str.toLowerCase().replace(" ","").split('').reverse().join('') ? true : false;
+const isPalindrome = (str) => str.toLowerCase().replace(" ","") === str.toLowerCase().replace(" ","").split('').reverse().join('');
