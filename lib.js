570e6ba0b099739466e42c9440b46e99ba2d309b
Ryann Flores <102124511+RynnFlrs@users.noreply.github.com>
2022-10-05 09:38:12 +0800

    Created square, farenheit, rectangleArea, and is Palindrome functions (#1)
    * Created square, farenheit, rectangleArea, and is Palindrome functions

---
+++ lib.js
@@ -0,0 +1,7 @@
+const square = (n) => Math.pow(n, 2);
+
+const fahrenheit = (celsius) => (celsius * 1.8) + 32;
+
+const rectangleArea = (a, b) => a * b;
+
+const isPalindrome = (str) => str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
