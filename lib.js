3e9da12d5986f4b0820f92f3bb41b0f2fdaba318
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-05 08:25:04 +0800

    added remove space feature to isPalindrome function

--- lib.js
+++ lib.js
@@ -4,4 +4,4 @@ const fahrenheit = (celsius) => (celsius * 1.8) + 32;
 
 const rectangleArea = (a, b) =>  area = a * b;
 
-const isPalindrome = (str) => str.toLowerCase() === str.toLowerCase().split('').reverse().join('') ? true : false;
+const isPalindrome = (str) => str.toLowerCase().replace(" ","") === str.toLowerCase().replace(" ","").split('').reverse().join('') ? true : false;
