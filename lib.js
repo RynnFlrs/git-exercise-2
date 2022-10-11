2501a670f0a00a980afe811123bc13a2d8862159
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-11 09:14:12 +0800

    Installed and applied prettier package

--- lib.js
+++ lib.js
@@ -1,14 +1,15 @@
 const square = (n) => Math.pow(n, 2);
 
-const fahrenheit = (celsius) => (celsius * 1.8) + 32;
+const fahrenheit = (celsius) => celsius * 1.8 + 32;
 
 const rectangleArea = (a, b) => a * b;
 
-const isPalindrome = (str) => str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+const isPalindrome = (str) =>
+  str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
 
 module.exports = {
-    square,
-    fahrenheit,
-    rectangleArea,
-    isPalindrome
-}
+  square,
+  fahrenheit,
+  rectangleArea,
+  isPalindrome,
+};
