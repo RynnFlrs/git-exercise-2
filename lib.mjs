cc1687e8f7b85e9221f699320ccbb79948886eb2
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-26 11:07:23 +0800

    Implemented ESM and ESLint

---
+++ lib.mjs
@@ -0,0 +1,7 @@
+export const square = (n) => n * n
+
+export const fahrenheit = (celsius) => celsius * 1.8 + 32
+
+export const rectangleArea = (a, b) => a * b
+
+export const isPalindrome = (str) => str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
